41fd3f4503d0cffa67a61f04cf5454fe77862843
xenoputtss <xenoputtss@gmail.com>
2015-11-17 20:59:12 -0500

    Step 1

---
+++ CarpMuffin-Demo-Win/Scenes/SceneNames.cs
@@ -0,0 +1,9 @@
+﻿namespace CarpMuffin_Demo_Win.Scenes
+{
+    public static class SceneNames
+    {
+        public static string Splash = "SplashScene";
+        public static string Title = "TitleScene";
+        public static string Demo1 = "Demo1Scene";
+    }
+}
